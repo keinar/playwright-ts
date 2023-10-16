86441fc8e04e948a8075acce93d8d31cbc614ce8
keinar <keinarelkayam@gmail.com>
2023-10-16 14:39:34 +0300

    update sanity test

--- tests/sanity.spec.ts
+++ tests/sanity.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
+test('sanity test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="username"]').press('Tab');
