fb326da32997872b67393764bc356720e912815c
keinar <keinarelkayam@gmail.com>
2023-10-22 14:44:14 +0300

    update sanity test structure

--- tests/LoginTests/positiveLogin.spec.ts
+++ tests/LoginTests/positiveLogin.spec.ts
@@ -1,39 +1,36 @@
-import {test} from "@playwright/test"
+import { test } from "@playwright/test";
 import LoginPage from "../../pages/loginPage";
 import UserCredentials from "../../helpers/UserCredentials";
-import { ErrorMessages } from "../../helpers/ErrorMessage";
 import ApplicationURL from "../../helpers/ApplicationURL";
 
 test.describe("Positive login scenarios", () => {
+  let loginPage: LoginPage;
 
-    let loginPage: LoginPage;
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+  });
 
-    test.beforeEach(async ({page}) => {
-        loginPage = new LoginPage(page);
-    })
+  test("Login with standart user", async ({ page }) => {
+    await loginPage.loginToApplication(
+      UserCredentials.STANDARD_USER,
+      UserCredentials.CORRECT_PASSWORD
+    );
+    await loginPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
+  });
 
-    test("Login with standart user",async ({page}) => {
-        await loginPage.loginToApplication(
-            UserCredentials.STANDARD_USER,
-            UserCredentials.CORRECT_PASSWORD
-          );
-          await loginPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
-    })
+  test("Login with problem user", async ({ page }) => {
+    await loginPage.loginToApplication(
+      UserCredentials.PROBLEM_USER,
+      UserCredentials.CORRECT_PASSWORD
+    );
+    await loginPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
+  });
 
-    test("Login with problem user",async ({page}) => {
-        await loginPage.loginToApplication(
-            UserCredentials.PROBLEM_USER,
-            UserCredentials.CORRECT_PASSWORD
-          );
-          await loginPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
-    })
-
-    test("Login with performance glitch usr",async ({page}) => {
-        await loginPage.loginToApplication(
-            UserCredentials.PERFORMANCE_GLITCH_USER,
-            UserCredentials.CORRECT_PASSWORD
-          );
-          await loginPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
-    })
-
-})
+  test("Login with performance glitch usr", async ({ page }) => {
+    await loginPage.loginToApplication(
+      UserCredentials.PERFORMANCE_GLITCH_USER,
+      UserCredentials.CORRECT_PASSWORD
+    );
+    await loginPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
+  });
+});
--- tests/sanity.spec.ts
+++ tests/sanity.spec.ts
@@ -4,47 +4,46 @@ import ApplicationURL from "../helpers/ApplicationURL";
 import ProductsPage from "../pages/ProductsPage";
 import LoginPage from "../pages/loginPage";
 
-test.describe("Sanity tests",async () => {
-  test.beforeEach(async () => {
-      
-  })
+test.describe("Sanity tests", async () => {
+  let loginPage: LoginPage;
+  let productsPage: ProductsPage;
 
-test("sanity test", async ({ page }) => {
-  const loginPage = new LoginPage(page);
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    productsPage = new ProductsPage(page);
+  });
 
-  await loginPage.loginToApplication(
-    UserCredentials.STANDARD_USER,
-    UserCredentials.CORRECT_PASSWORD
-  );
+  test("sanity test", async ({ page }) => {
+    await loginPage.loginToApplication(
+      UserCredentials.STANDARD_USER,
+      UserCredentials.CORRECT_PASSWORD
+    );
 
-  loginPage.validatePageUrl(`${ApplicationURL.BASE_URL}/inventory.html`);
+    await loginPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
 
-  await page
-    .locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]')
-    .click();
+    await page
+      .locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]')
+      .click();
 
-  await page.locator("a").filter({ hasText: "1" }).click();
-  await page.locator('[data-test="checkout"]').click();
-  await page.locator('[data-test="firstName"]').click();
-  await page.locator('[data-test="firstName"]').fill("keinar");
-  await page.locator('[data-test="firstName"]').press("Tab");
-  await page.locator('[data-test="lastName"]').fill("elkayam");
-  await page.locator('[data-test="lastName"]').press("Tab");
-  await page.locator('[data-test="postalCode"]').fill("123");
-  await page.locator('[data-test="continue"]').click();
-  await page.locator('[data-test="finish"]').click();
-  await page.locator('[data-test="back-to-products"]').click();
+    await page.locator("a").filter({ hasText: "1" }).click();
+    await page.locator('[data-test="checkout"]').click();
+    await page.locator('[data-test="firstName"]').click();
+    await page.locator('[data-test="firstName"]').fill("keinar");
+    await page.locator('[data-test="firstName"]').press("Tab");
+    await page.locator('[data-test="lastName"]').fill("elkayam");
+    await page.locator('[data-test="lastName"]').press("Tab");
+    await page.locator('[data-test="postalCode"]').fill("123");
+    await page.locator('[data-test="continue"]').click();
+    await page.locator('[data-test="finish"]').click();
+    await page.locator('[data-test="back-to-products"]').click();
+  });
+
+  test("negative login test", async ({ page }) => {
+    await loginPage.loginToApplication(
+      UserCredentials.STANDARD_USER,
+      UserCredentials.CORRECT_PASSWORD
+    );
+    await productsPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
+    await productsPage.validateTitle("Products");
+  });
 });
-
-test("negative login test", async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.loginToApplication(
-    UserCredentials.STANDARD_USER,
-    UserCredentials.CORRECT_PASSWORD
-  );
-  const productsPage = new ProductsPage(page);
-  await productsPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
-  await productsPage.validateTitle('Products');
-});
-
-})
